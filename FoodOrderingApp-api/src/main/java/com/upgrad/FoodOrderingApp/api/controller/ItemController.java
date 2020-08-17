b3655dd23876feeb5d230b26d9a5e3c6585b2c8d
Anil Narayanan <narayanan.anil@gmail.com>
2020-08-17 23:37:47 +0530

    Applied bug fix.

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/ItemController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/ItemController.java
@@ -1,4 +1,58 @@
 package com.upgrad.FoodOrderingApp.api.controller;
 
+import com.upgrad.FoodOrderingApp.api.model.ItemList;
+import com.upgrad.FoodOrderingApp.api.model.ItemListResponse;
+import com.upgrad.FoodOrderingApp.service.businness.ItemService;
+import com.upgrad.FoodOrderingApp.service.businness.RestaurantService;
+import com.upgrad.FoodOrderingApp.service.entity.ItemEntity;
+import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import com.upgrad.FoodOrderingApp.service.exception.RestaurantNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import java.util.UUID;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/item")
 public class ItemController {
+
+    @Autowired
+    ItemService itemService;
+
+    @Autowired
+    RestaurantService restaurantService;
+
+    /**
+     *
+     * @param restaurantUuid
+     * @return
+     * @throws RestaurantNotFoundException
+     */
+    @CrossOrigin
+    @RequestMapping(method = RequestMethod.GET,path = "/restaurant/{restaurant_id}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<ItemListResponse> getTopFiveItemsByPopularity (@PathVariable(value = "restaurant_id")final String restaurantUuid) throws RestaurantNotFoundException {
+
+        List<ItemEntity> itemEntityList;
+        RestaurantEntity restaurantEntity;
+
+        restaurantEntity = restaurantService.restaurantByUUID(restaurantUuid);
+        itemEntityList = itemService.getItemsByPopularity(restaurantEntity);
+        List<ItemEntity> itemEntities = itemService.getItemsByPopularity(restaurantEntity);
+
+        ItemListResponse itemListResponse = new ItemListResponse();
+        itemEntityList.forEach(itemEntity -> {
+            ItemList itemList = new ItemList()
+                    .id(UUID.fromString(itemEntity.getUuid()))
+                    .itemName(itemEntity.getItemName())
+                    .price(itemEntity.getPrice())
+                    .itemType(ItemList.ItemTypeEnum.fromValue(itemEntity.getType().getValue()));
+            itemListResponse.add(itemList);
+        });
+
+        return new ResponseEntity<ItemListResponse>(itemListResponse, HttpStatus.OK);
+    }
 }
